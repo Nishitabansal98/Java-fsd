9974fc25653f7d1c22b95ed849e108729993ba63
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-07-20 20:39:01 +0530

    changes

---
+++ Assisted Practice Project/src/accessSpecifiers.java
@@ -0,0 +1,11 @@
+
+public class accessSpecifiers {
+	public static void main(String[] args) {
+		//default
+		System.out.println("Dafault Access Specifier");
+		defAccessModifier obj = new defAccessModifier(); 		  
+        obj.display(); 
+
+	}
+}
+
---
+++ Assisted Practice Project/src/defAccessModifier.java
@@ -0,0 +1,8 @@
+
+public class defAccessModifier {
+
+	  void display() 
+	     { 
+	         System.out.println("You are using defalut access specifier"); 
+	     } 
+	} 
